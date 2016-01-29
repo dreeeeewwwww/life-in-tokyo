b651f5c95da808803fd7f3f337b76182d7e3f9a0
dreeeeewwwww <andrew.david.gardner@gmail.com>
2016-01-29 16:50:36 -0500

    archive 2008 version

---
+++ archive/2008/menu.php
@@ -0,0 +1,25 @@
+<div class="nav_wrap">
+
+<ul class="nav">
+	
+	<li style="margin-bottom:10px;"><a class="foo" style="border:none;" href="/"><img src="img/dg.png" style="border:none;" /></a></li>
+	
+	<li class="graphics"><a href="graphics">Graphics / Web</a></li>
+	
+	<!-- <li class="nav_bar"><a class="menu" href="/test/"><strike>Typography</strike></a> Coming Soon</li> -->
+	
+	<li class="photography"><a href="photography">Photography</a></li>
+	
+	<li class="log" style="margin-bottom:50px;"><a href="info">Info</a></li>
+	
+	<li>202.455.6497</li>
+	
+	<li>hello@<br />drewgardner.net</li>
+	
+</ul>
+
+<img src="img/bottom_shadow.png" style="margin:0 0 0 7px; padding:0;" />
+
+</div>
+
+
