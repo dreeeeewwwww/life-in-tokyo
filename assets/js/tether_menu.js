66e0463662f8916a160a7173db6dec4e18e14f1d
Drew <andrew.david.gardner@gmail.com>
2016-12-21 22:23:16 -0500

    something with tether

--- assets/js/tether_menu.js
+++ assets/js/tether_menu.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
-  new Tether({
-    element: '.element',
-    target: '.target',
-    attachment: 'top left',
-    targetAttachment: 'top right',
-    constraints: [
-      {
-        to: 'window',
-        attachment: 'together',
-        pin: true
-      }
-    ],
-    optimizations: {
-      moveElement: false
-    }
-  });
+
+            new Tether({
+                element: '.element',
+                target: '.target',
+                attachment: 'top left',
+                targetAttachment: 'top right',
+                constraints: [{
+                    to: 'window',
+                    pin: true
+                }]
+            });
+        
 });
