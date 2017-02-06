267b7509e072aaca157027b39e6592176287d6f0
Admin <andrew.david.gardner@gmail.com>
2017-02-06 01:22:19 -0500

    tether is the worst

--- assets/js/tether_menu.js
+++ assets/js/tether_menu.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
 
-  tether = new Tether({
+  new Tether({
       element: '.pin-element',
       target: '.pin-target',
       attachment: 'top left',
       targetAttachment: 'top right',
+      offset: '0 20px',
+      enabled: true,
       constraints: [
         {
           to: 'window',
+          attachment: 'together',
           pin: true
         }
       ]
   });
-  tether.position();
 
 });
